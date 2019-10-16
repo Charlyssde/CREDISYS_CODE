6e1258a73e5ba4cde47d0b122574bed65bba3358
Carlos Carrillo <texcharly9@gmail.com>
2019-10-16 18:03:00 -0500

    GUI dashboard administrador

--- CREDISYS/Views/AdministrarUsuarios.xaml.cs
+++ CREDISYS/Views/AdministrarUsuarios.xaml.cs
@@ -1,5 +1,4 @@
-﻿using CREDISYS.Views;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,26 +10,20 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CREDISYS.Properties;
 
-namespace CREDISYS
+namespace CREDISYS.Views
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Lógica de interacción para Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AdministrarUsuarios : Window
     {
-        public MainWindow()
+        public AdministrarUsuarios()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            DashboardAdmin dashboard_Admin = new DashboardAdmin();
-            dashboard_Admin.Show();
-            this.Close();
+            
         }
     }
 }
---
+++ CREDISYS/Views/Dashboard_Admin.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CREDISYS.Properties;
+
+namespace CREDISYS.Views
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class DashboardAdmin: Window
+    {
+        public DashboardAdmin()
+        {
+            InitializeComponent();
+            lblNombre.Content = "Carlos de Jesús Carrillo Ceballos";
+            
+        }
+
+        private void btnAdminUsuarios_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdminCatalogos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCobranza_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSolicitud_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDictamen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCliente_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
