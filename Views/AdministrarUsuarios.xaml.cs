aa6b4a39434bb74746e5f23c732a3d88b612f304
Carlos Carrillo <texcharly9@gmail.com>
2019-11-09 16:28:23 -0600

    Mi trabajo

--- Views/AdministrarUsuarios.xaml.cs
+++ Views/AdministrarUsuarios.xaml.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;  
+using System.Windows.Controls;
+
 namespace CREDISYS.Views
 {
     /// <summary>
@@ -15,14 +16,23 @@ namespace CREDISYS.Views
     {
         Usuario selected = null;
         List<Rol> roles;
-        public AdministrarUsuarios()
+
+        Usuario usuario;
+
+        DashboardAdmin dashboardAdmin;
+
+        public AdministrarUsuarios(Usuario usuario, DashboardAdmin dashboardAdmin)
         {
             InitializeComponent();
-            
+            cargarInfo(usuario, dashboardAdmin);
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
         {
+            DashboardAdmin dashboardAdmin = new DashboardAdmin(this.usuario);
+            dashboardAdmin.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            dashboardAdmin.Show();
+            closeWindow();
 
         }
 
@@ -34,23 +44,41 @@ namespace CREDISYS.Views
                 {
                     roles = db.Rols.ToList<Rol>();
                 }
-                EditarUsuario screen = new EditarUsuario(selected, roles);
-                screen.ShowDialog();
+                EditarUsuario editarUsuario = new EditarUsuario(selected, roles);
+                editarUsuario.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                editarUsuario.ShowDialog();
             }
             limpiarInformacion();
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
-           
+           MessageBoxResult result =  MessageBox.Show("¿Desea eliminar?", "Confirmación", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                if (selected != null)
+                {
+                    try
+                    {
+                        using (DBEntities db = new DBEntities())
+                        {
+                            db.Usuarios.Remove(selected);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show(Settings.Default.MensajeErrorBD);
+                    }
+                }
+            }
+            limpiarInformacion();
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             RegistrarUsuario screen = new RegistrarUsuario();
-            screen.WindowStartupLocation = this.WindowStartupLocation;
+            screen.WindowStartupLocation = WindowStartupLocation.CenterScreen;
             screen.ShowDialog();
-
             limpiarInformacion();
         }
 
@@ -64,47 +92,68 @@ namespace CREDISYS.Views
                 if (username.Equals("")) 
                 {
                     MessageBox.Show(Settings.Default.MensajeCamposVacios);
-                    txtNombre.Text = "";
-                    txtUsername.Text = "";
-                    txtRol.Text = "";
-                    txtPassword.Password = "";
+                    limpiarInformacion();
                 }
                 else
                 {
                     try
                     {
                         selected = db.Usuarios.Where(b => b.username.Equals(username)).FirstOrDefault();
+                        if (selected != null)
+                        {
+                            txtNombre.Text = selected.nombre;
+                            txtUsername.Text = selected.username;
+                            txtRol.Text = selected.Rol.rol1;
+                            txtPassword.Password = "" + selected.password;
+
+                            if (selected.username == this.usuario.username)
+                            {
+                                MessageBox.Show("Este usuario no se puede editar o eliminar... Porque eres tú");
+                            }
+                            else
+                            {
+                                btnDelete.IsEnabled = true;
+                                btnEdit.IsEnabled = true;
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show(Settings.Default.MensajeNoEncontrado);
+                        }
                     }
                     catch (Exception)
                     {
                         MessageBox.Show(Settings.Default.MensajeErrorBD);
                     }
-                    if (selected != null)
-                    {
-                        txtNombre.Text = selected.nombre;
-                        txtUsername.Text = selected.username;
-                        txtRol.Text = selected.Rol.rol1;
-                        txtPassword.Password = "" + selected.password;
-
-                        btnDelete.IsEnabled = true;
-                        btnEdit.IsEnabled = true;
-                    }
-                    else
-                    {
-                        MessageBox.Show(Settings.Default.MensajeNoEncontrado);
-                    }
+                    
                 }
             }
             txtBusqueda.Text = "";
         }
- 
-    private void limpiarInformacion()
+
+        private void cargarInfo(Usuario usuario, DashboardAdmin dashboardAdmin)
         {
+            this.usuario = usuario;
+            this.dashboardAdmin = dashboardAdmin;
+        }
+
+        private void limpiarInformacion()
+        {
+            selected = null;
+
+            this.btnDelete.IsEnabled = false;
+            this.btnEdit.IsEnabled = false;
+
             txtNombre.Text = "";
             txtPassword.Password = "";
             txtUsername.Text = "";
             txtRol.Text = "";
             
+        }
+
+        private void closeWindow()
+        {
+            this.Close();
         }
     }
 
---
+++ Views/PopUp/EncuestaSolicitud.xaml.cs
@@ -0,0 +1,114 @@
+﻿using CREDISYS.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CREDISYS.Views.PopUp
+{
+    /// <summary>
+    /// Lógica de interacción para EncuestaSolicitud.xaml
+    /// </summary>
+    public partial class EncuestaSolicitud : Window
+    {
+        public String comentarios;
+
+        private Solicitud solicitud;
+        public EncuestaSolicitud(Solicitud solicitud)
+        {
+            InitializeComponent();
+            cargarCbParentesco();
+            this.solicitud = solicitud;
+        }
+
+        private void rbNoUno_Checked(object sender, RoutedEventArgs e)
+        {
+            txtPeriodoFinal.IsEnabled = false;
+            txtPeriodoInicio.IsEnabled = false;
+            txtPuestoUno.IsEnabled = false;
+            txtPuestoUno.Text = "";
+            txtPeriodoFinal.Text = "";
+            txtPeriodoInicio.Text = "";
+        }
+
+        private void rbSiUno_Checked(object sender, RoutedEventArgs e)
+        {
+            txtPeriodoFinal.IsEnabled = true;
+            txtPeriodoInicio.IsEnabled = true;
+            txtPuestoUno.IsEnabled = true;
+        }
+
+        private void rbNoDos_Checked(object sender, RoutedEventArgs e)
+        {
+            txtPuestoDos.IsEnabled = false;
+            txtPeriodoFinalDos.IsEnabled = false;
+            txtPeriodoInicioDos.IsEnabled = false;
+            txtApellidoMaterno.IsEnabled = false;
+            txtApellidoPaterno.IsEnabled = false;
+            txtNombre.IsEnabled = false;
+        }
+
+        private void rbSiDos_Checked(object sender, RoutedEventArgs e)
+        {
+            txtPuestoDos.IsEnabled = true;
+            txtPeriodoFinalDos.IsEnabled = true;
+            txtPeriodoInicioDos.IsEnabled = true;
+            txtApellidoMaterno.IsEnabled = true;
+            txtApellidoPaterno.IsEnabled = true;
+            txtNombre.IsEnabled = true;
+        }
+
+        private void btnComentarios_Click(object sender, RoutedEventArgs e)
+        {
+            Comentarios comentarios = new Comentarios(this);
+            comentarios.ShowDialog();
+
+        }
+
+        private void btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO
+            //Generar los documentos que surgen de aquí, la tabla de pagos, etc
+            closeWindow();
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (DBEntities db = new DBEntities())
+                {
+                    Solicitud cancelada = db.Solicituds.Where(b => b.folio == solicitud.folio).SingleOrDefault();
+                    cancelada.estatus = Settings.Default.SolicitudEstatus2;
+                    db.SaveChanges();
+                    closeWindow();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(Settings.Default.MensajeErrorBD);
+            }
+                
+        }
+
+        private void cargarCbParentesco()
+        {
+            String[] lista = { "Padre", "Hermano", "Hijo", "Abuelo", "Tio", "Primo", "Cuñado", "Suegro", "Nuera/Yerno", "Conyuge"};
+            cbParentensco.ItemsSource = lista;
+        }
+
+        private void closeWindow()
+        {
+            this.Close();
+        }
+    }
+}
