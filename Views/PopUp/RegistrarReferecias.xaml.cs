872f16193e61a8952ca9287a8bc6f7c617e16536
Aline Montserrat <alinemhdez@gmail.com>
2019-11-11 14:50:06 -0600

    Referencias

--- Views/PopUp/RegistrarReferecias.xaml.cs
+++ Views/PopUp/RegistrarReferecias.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CREDISYS.Properties;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,36 @@ namespace CREDISYS.Views.PopUp
         public RegistrarReferecias()
         {
             InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            closeWindow();
+        }
+
+        private void btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            using(DBEntities db = new DBEntities())
+            {
+                try
+                {
+                    if (txt_name1.Text.Equals("") || txt_name2.Text.Equals("") || txtRelacion.Text.Equals("")
+                         || txtRelacion2.Text.Equals("") || txtDir.Text.Equals("") || txtDir2.Text.Equals("") || txtTel.Text.Equals("") || 
+                         txtTel2.Text.Equals("") || txtHorario.Text.Equals("") || txtHorario2.Text.Equals(""))
+                    {
+                        MessageBox.Show(Settings.Default.MensajeCamposVacios);
+                    }
+                    else
+                    {
+                        Referencia nueva = new Referencia();
+
+                    }
+                }
+             }
+        }
+        private void closeWindow()
+        {
+            this.Close();
         }
     }
 }
